c137ec16d381fe14260a00de4a36cab52f7fa373
Smit Patel <smitpat2000@gmail.com>
2020-03-26 22:06:59 +0530

    Add password and its attribute

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,15 @@
 class User < ApplicationRecord
+
   has_many :articles
   before_save { self.email = email.downcase }
+
   validates :username, presence: true, length: { minimum: 3, maximum: 25 },
             uniqueness: { case_sensitive: false }
+
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 100 },
             uniqueness: { case_sensitive: false },
             format: { with: VALID_EMAIL_REGEX }
+
+  has_secure_password
 end
