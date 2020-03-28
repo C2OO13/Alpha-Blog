667cad1f58f347f6bac0b93e9a850271add9fc60
Smit Patel <smitpat2000@gmail.com>
2020-03-28 20:36:37 +0530

    Pagination implemented

--- Gemfile
+++ Gemfile
@@ -27,6 +27,8 @@ gem 'bcrypt', '~> 3.1.7'
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
 
+gem 'will_paginate'
+gem 'bootstrap-will_paginate'
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.2', require: false
 
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,35 +1,34 @@
+# frozen_string_literal: true
 class ArticlesController < ApplicationController
 
-  before_action :set_article , only: [:edit, :show ,:update, :destroy]
+  before_action :set_article , only: %i[edit show update destroy]
 
   def index
-    @articles = Article.all
+    @articles = Article.paginate(page: params[:page], per_page: 2)
   end
 
   def new
     @article = Article.new
-end
+  end
 
   def create
     @article = Article.new(article_params)
     @article.user = User.first
     if @article.save
-      flash[:notice] = "Article successfully created!"
+      flash[:notice] = 'Article successfully created!'
       redirect_to article_path(@article)
     else
       render 'new'
     end
   end
 
-  def show
-  end
+  def show; end
 
-  def edit
-  end
+  def edit; end
 
   def update
     if @article.update(article_params)
-      flash[:notice] = "Article successfully updated!"
+      flash[:notice] = 'Article successfully updated!'
       redirect_to article_path(@article)
     else
       render :edit
@@ -38,7 +37,7 @@ end
 
   def destroy
     if @article.destroy
-      flash[:notice] = "Article successfully deleted!"
+      flash[:notice] = 'Article successfully deleted!'
       redirect_to articles_path
     else
       render :edit
